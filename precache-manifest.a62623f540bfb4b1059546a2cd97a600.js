18f2813ce0d092c3e6101ab055baa5aea117d954
Willem-Aart van Dorpen <mail@willem-aart.nl>
2019-03-21 10:04:38 +0100

    Updates

---
+++ precache-manifest.a62623f540bfb4b1059546a2cd97a600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6f98735da372194a77e",
+    "url": "/static/js/main.aa3dc403.chunk.js"
+  },
+  {
+    "revision": "02d383ccbb76762e467b",
+    "url": "/static/js/2.cfd05851.chunk.js"
+  },
+  {
+    "revision": "a6f98735da372194a77e",
+    "url": "/static/css/main.e6257bfd.chunk.css"
+  },
+  {
+    "revision": "02d383ccbb76762e467b",
+    "url": "/static/css/2.2a3d6a64.chunk.css"
+  },
+  {
+    "revision": "ea6cd9644e7d7d6f0be722b1252a0bf4",
+    "url": "/index.html"
+  }
+];
